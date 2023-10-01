10b2b3daab28c392aa9c68f479f85a0dfb8c6028
anas-cd <anas.cd.97@gmail.com>
2023-10-02 00:36:27 +0300

    build: well this journey starts here folks :D
    main components of this commit:
    - add Vue's created project
    - add 'H-dister.sh' shell binary; this is used to configure the
    contents of the built 'dist' folder to suit hostinger's environment
    - configure SCSS preprocessing for Vue and add some global SCSS
    styling template
    - configure linter rules
    - add .htaccess rewrite rules for Hostinger in 'H-dist' folder
    
    let the fun begin.

---
+++ H-dister.sh
@@ -0,0 +1,47 @@
+#!/bin/sh
+# bash file to be run on the root directory
+
+# adding the date variable
+now=`date`
+
+echo " -- Requested At $now -- "
+
+echo "-- BUILDING APP FOR PRODUCTION --"
+npm run lint
+npm run build
+
+echo "-- ARRANGING FILES --"
+
+#checking if H-dist folder is available 
+DIR="./H-dist"
+
+if [ -d "$DIR" ]; then
+  ### exists ###
+  echo "$DIR located"
+else
+  ###  does NOT exists ###
+  mkdir -p "$DIR"
+  echo "$DIR created"
+fi
+
+# starting shopt extglob
+shopt -s extglob
+# change directory to Hostinger's deployment folder
+cd "$DIR/"
+# removing all folders 
+rm -r */
+# removing all files except for rewrite rules 
+rm -v !(".htaccess")
+# ending shopt extglob
+shopt -u extglob
+# coppying all files from the dist folder to current folder 
+cp -r ../dist/* ./
+
+echo "-- PREPARING FILES FOR HOSTINGER --"
+
+# changing index from html to php to work on Hostinger's server
+mv index.html index.php
+
+cd ..
+
+echo "-- READY TO COMMIT AND PUSH :D --"
