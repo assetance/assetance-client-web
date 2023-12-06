2f3e99280ec6487befddee987ae61e49e52c85fb
anas-cd <anas.cd.97@gmail.com>
2023-12-06 05:43:46 +0300

    feat(frontend): add scroll behavior changes

--- src/router/index.js
+++ src/router/index.js
@@ -35,6 +35,13 @@ const router = createRouter({
       // }),
     },
   ],
+  scrollBehavior(to, from, savedPosition) {
+    if (savedPosition) {
+      return savedPosition
+    } else {
+      return { top: 0, behavior: 'smooth' }
+    }
+  },
 });
 
 router.beforeEach((to, from, next) => {
