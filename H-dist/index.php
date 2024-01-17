5829bc45b2c9984901100650c341f4e849ec8673
anas-cd <anas.cd.97@gmail.com>
2024-01-17 20:55:12 +0300

    feat(SEO): add default minimum SEO tags

--- H-dist/index.php
+++ H-dist/index.php
@@ -5,7 +5,30 @@
   <meta charset="UTF-8">
   <link rel="icon" href="./favicon.ico">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <!-- HTML Meta Tags -->
   <title>Assetance</title>
+  <meta name="description" content="">
+  <meta name="keywords" content="assetance, real estate investing, fractional real estate investing, fractional investing">
+  <meta name="description" content="Simple Real Estate Investing Starting From $10">
+  <meta name="copyright" content="All rights reserved">
+  <!-- openGraph Meta Tags -->
+  <meta property="og:url" content="https://dev.assetance.com/">
+  <meta property="og:type" content="website">
+  <meta property="og:title" content="Assetance">
+  <meta property="og:description" content="Simple Real Estate Investing Starting From $10">
+  <meta property="og:image" content="">
+  
+  <!-- Twitter Meta Tags -->
+  <meta name="twitter:card" content="summary">
+  <meta property="twitter:domain" content="dev.assetance.com">
+  <meta property="twitter:url" content="https://dev.assetance.com/">
+  <meta name="twitter:title" content="Assetance">
+  <meta name="twitter:description" content="Simple Real Estate Investing Starting From $10">
+  <meta name="twitter:image" content="">
+  
+  <!-- dev only -->
+  <link rel="canonical" href="https://dev.assetance.com/"> 
+  <!-- dev only -->
   <script type="module" crossorigin src="/assets/index-17b6fad9.js"></script>
   <link rel="stylesheet" href="/assets/index-95dff388.css">
 </head>
